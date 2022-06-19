91a9694961a1ddc909e83fb50c6e2ea5ca84ba93
samuel martins <samuel.martins4.sm@gmail.com>
2022-06-19 21:15:44 +0300

    chore: create business and post models

--- neighborhood/models.py
+++ neighborhood/models.py
@@ -8,4 +8,17 @@ class Neighborhood(models.Model):
     occupants = models.IntegerField(default=0, blank=True)
     admin = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True, default=None)
 
+
+
+class Business(models.Model):
+    name = models.CharField(max_length=1000)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    hood = models.ForeignKey(Neighborhood, on_delete=models.DO_NOTHING)
+    email = models.EmailField(max_length=1000)
+
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    content = models.TextField()
     
--- users/models.py
+++ users/models.py
@@ -5,4 +5,5 @@ from neighborhood.models import Neighborhood
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='hood-default.png', upload_to='hood_profile_images/')
     hood = models.ForeignKey(Neighborhood, on_delete=models.DO_NOTHING)
