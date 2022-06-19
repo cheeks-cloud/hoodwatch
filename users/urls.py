aa1b828c2375fc6dc919fc3dc12c87ced286bbbd
samuel martins <samuel.martins4.sm@gmail.com>
2022-06-19 16:23:10 +0300

    feature: add user creation feature

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,6 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('signup/', views.signup, name='users-signup'),
-    path('signin/', auth_views.LoginView.as_view(template_name='users/signin'), name='users-signin'),
+    path('signin/', auth_views.LoginView.as_view(template_name='users/signin.html'), name='users-signin'),
     path('signout/', auth_views.LogoutView.as_view(), name='users-signout')
 ]
